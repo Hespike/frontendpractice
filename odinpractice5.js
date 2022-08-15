63950a6a8f98a0275140f1177aa708777290e659
Sárdi András <sardi.andras@gmail.com>
2022-08-15 13:32:51 +0200

    Create odinpractice5.js

---
+++ odinpractice5.js
@@ -0,0 +1,9 @@
+/* Write a function called add7 that takes one number and returns that number + 7. */
+
+let examplenumber = 3;
+
+function add7(number) {
+  return number+7;
+}
+
+add7(examplenumber)
