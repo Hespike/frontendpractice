d69cab6371723f13199334f9029c183758289e26
Sárdi András <sardi.andras@gmail.com>
2022-08-15 13:34:59 +0200

    Create odinpractice6.js

---
+++ odinpractice6.js
@@ -0,0 +1,10 @@
+/* Write a function called multiply that takes 2 numbers and returns their product. */
+
+let firstexamplenumber = 3;
+let secondexamplenumber = 4;
+
+function multiply(firstnumber, secondnumber) {
+  return firstnumber * secondnumber;
+}
+
+multiply(firstexamplenumber, secondexamplenumber);
