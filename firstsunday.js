9bd5af04f7e81651bee46a0c245ac332b4d6e695
Sárdi András <sardi.andras@gmail.com>
2022-08-25 21:53:00 +0200

    Update firstsunday.js
    Véletlen var-t írtam.

--- firstsunday.js
+++ firstsunday.js
@@ -4,7 +4,7 @@
 
 //Ha mindegyiket szeretném.
 
-for (var year = 2014; year <= 2050; year++) {
+for (let year = 2014; year <= 2050; year++) {
   let day = new Date(year, 0, 1);
   if (day.getDay() === 0)
     console.log(`1st January is being a Sunday ${year}`);
@@ -12,7 +12,7 @@ for (var year = 2014; year <= 2050; year++) {
 
 //Ha csak egyet.
 
-for (var year = 2014; year <= 2050; year++) {
+for (let year = 2014; year <= 2050; year++) {
   let day = new Date(year, 0, 1);
   if (day.getDay() === 0) {
     console.log(`1st January is being a Sunday ${year}`);
