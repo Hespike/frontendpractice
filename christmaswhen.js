9ac79e048ad3cba1243e3823f5f775dbfd506db6
Sárdi András <sardi.andras@gmail.com>
2022-08-27 17:32:16 +0200

    Update christmaswhen.js

--- christmaswhen.js
+++ christmaswhen.js
@@ -4,9 +4,9 @@ const currentDay = new Date();
 const christmas = new Date(currentDay.getFullYear(), 11, 26);
 
 if (currentDay.getMonth() == 11 && currentDay.getDate() > 25) {
-christmas.setFullYear(christmas.getFullYear() + 1);
+  christmas.setFullYear(christmas.getFullYear() + 1);
 }
 // azért kell szorozni ezerrel, mert milisec-et használ a kivonásunk később.
-const day = 1000*60*60*24;
+const day = 1000 * 60 * 60 * 24;
 
 console.log(` ${Math.ceil((christmas.getTime()-currentDay.getTime())/(day))} nap van még Karácsonyig.`);
