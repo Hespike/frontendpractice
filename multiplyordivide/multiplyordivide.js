6e27fdfa7e2ac09d7ae26ea87854620682f9a6ee
Sárdi András <sardi.andras@gmail.com>
2022-08-28 19:17:55 +0200

    Create multiplyordivide.js

---
+++ multiplyordivide/multiplyordivide.js
@@ -0,0 +1,13 @@
+function multiplyBy()
+{
+        firstNumber = document.getElementById("firstNumber").value;
+        secondNumber = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = firstNumber * secondNumber;
+}
+
+function divideBy() 
+{ 
+        firstNumber = document.getElementById("firstNumber").value;
+        secondNumber = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = firstNumber / secondNumber;
+}
