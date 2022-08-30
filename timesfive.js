f4d7d6ec3eabcee4a7070d1bb42c6de8b0273d1e
Sárdi András <sardi.andras@gmail.com>
2022-08-30 14:54:47 +0200

    Create timesfive.js

---
+++ timesfive.js
@@ -0,0 +1,14 @@
+/*Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+timesFive should be a function
+timesFive(5) should return 25
+timesFive(2) should return 10
+timesFive(0) should return 0
+*/
+
+function timesFive(num) {
+  return num * 5;
+}
+
+timesFive(5);
+timesFive(2);
+timesFive(0);
