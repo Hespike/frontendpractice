5d8241b7a5a7c2c9c2b8644b594c7cbed0215ad3
Sárdi András <sardi.andras@gmail.com>
2022-09-05 10:28:41 +0200

    Create changearray.js

---
+++ changearray.js
@@ -0,0 +1,24 @@
+// An array is declared as const s = [5, 7, 2]. Change the array to [2, 5, 7] using various element assignments.
+
+const s = [5, 7, 2];
+function editInPlace() {
+
+s.pop();
+s.pop();
+s.pop();
+s.push(2);
+s.push(5);
+s.push(7);
+}
+editInPlace();
+
+/*
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+editInPlace();
+*/
