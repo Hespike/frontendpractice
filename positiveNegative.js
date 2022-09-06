a8fdf3d820d25fd76212958a948707a0ee38c9a6
Sárdi András <sardi.andras@gmail.com>
2022-09-06 22:43:19 +0200

    Create positiveNegative.js

---
+++ positiveNegative.js
@@ -0,0 +1,11 @@
+/*
+Write a JavaScript program to check from two given integers, whether one is positive and another one is negative.
+*/
+
+function positiveNegative(num1, num2) {
+  return ((num1 >= 0 && num2 < 0) || (num2 >= 0 && num1 < 0));
+}
+
+console.log(positiveNegative(10, -20));
+console.log(positiveNegative(10, 20));
+console.log(positiveNegative(-10, -20));
