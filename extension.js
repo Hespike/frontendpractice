c27c97dd5bf9ce272dbb374da99f5b9b02dfba31
Sárdi András <sardi.andras@gmail.com>
2022-09-06 13:17:28 +0200

    Create extension.js

---
+++ extension.js
@@ -0,0 +1,6 @@
+//Write a JavaScript exercise to get the extension of a filename.
+
+filename = "example.php"
+console.log(filename.split('.').pop());
+filename = "picture.jpeg"
+console.log(filename.split('.').pop());
