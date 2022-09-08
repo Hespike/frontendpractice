ba4aaa0778d5d00d16507dad38885c101c14b794
Sárdi András <sardi.andras@gmail.com>
2022-09-08 14:53:35 +0200

    Create firstCharacters.js

---
+++ firstCharacters.js
@@ -0,0 +1,15 @@
+/*
+Write a JavaScript program to create a new string from a given string taking the first 3 characters and added at both the front and back. The string length must be 3 or more.
+*/
+
+function firstCharacters(string) {
+  if (string.length >= 3) {
+    return string.charAt(0) + string.charAt(1) + string.charAt(2) + string + string.charAt(0) + string.charAt(1) + string.charAt(2);
+  } else {
+    return "Your string should be at least 3 characters long.";
+  }
+}
+
+console.log(threeCharacters("Hello"));
+console.log(threeCharacters("Example"));
+console.log(threeCharacters("Le"));
