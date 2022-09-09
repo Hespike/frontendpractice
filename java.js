beb84c4192ede10f12bb1f7683d8191e3a4e7e8e
Sárdi András <sardi.andras@gmail.com>
2022-09-09 10:23:52 +0200

    Create java.js

---
+++ java.js
@@ -0,0 +1,8 @@
+/* Write a JavaScript program to check whether a string is 'Java' and false otherwise! */
+
+function java(string){
+return string === "java";
+}
+
+console.log(java("java"));
+console.log(java("ello"));
