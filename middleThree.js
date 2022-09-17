30ff9441eacbef325a8a71c892978287f0bba7a8
Sárdi András <sardi.andras@gmail.com>
2022-09-17 22:55:12 +0200

    Update middleThree.js

--- middleThree.js
+++ middleThree.js
@@ -4,12 +4,12 @@ Write a JavaScript program to create a string using the middle three characters 
 
 function middleThree(str) {
   if (str.length >= 3 && str.length % 2 === 1) {
-    return str.slice(str.length / 2 - 1,str.length / 2 + 2);
+    return str.slice(str.length / 2 - 1, str.length / 2 + 2);
   } else {
     return "Nem jó paraméter.";
   }
 }
 
 console.log(middleThree("goodparameter"));
-console.log(middleThree("hello"));
+console.log(middleThree("hel"));
 console.log(middleThree("hell"));
