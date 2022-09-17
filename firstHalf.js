abdadbda0ddb318cd441db4ae4e09b8d07443a0c
Sárdi András <sardi.andras@gmail.com>
2022-09-17 22:17:08 +0200

    Update firstHalf.js

--- firstHalf.js
+++ firstHalf.js
@@ -5,7 +5,7 @@ Write a JavaScript program to extract the first half of a string of even length.
 function firstHalf(str) {
 
   if (str.length % 2 === 0) {
-    return str.substring(0, str.length / 2);
+    return str.substring(0, str.length / 2); //str.slice(0, str.length / 2);
   } else {
     return "Nem megfelelő paraméter.";
   }
