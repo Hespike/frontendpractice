bc7c61f1fce6314338bbfc64b98e7c17657b435d
Sárdi András <sardi.andras@gmail.com>
2022-09-20 11:43:41 +0200

    Create sumArray.js

---
+++ sumArray.js
@@ -0,0 +1,10 @@
+/*
+Write a JavaScript program to compute the sum of three elements of a given array of integers of length 3.
+*/
+
+function sumArray(array) {
+
+return array[0] + array[1] + array[2];
+}
+
+console.log(sum([3,4,5]));
